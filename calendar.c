c491517c58a49d6f48eb173617435c7f982b8486
Angel Nevarez <angel23@cs.utexas.edu>
2021-12-06 02:32:35 -0600

    日本語のクラス

--- calendar.c
+++ calendar.c
@@ -9,8 +9,6 @@
 int days_in_month[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 char *months[] = {" ","\n\n\n一月", "\n\n\n二月","\n\n\n三月","\n\n\n四月","\n\n\n五月","\n\n\n六月","\n\n\n七月", "\n\n\n八月","\n\n\n九月",
                 "\n\n\n十月","\n\n\n十一月", "\n\n\n十二月"};
-char *japanDays[] = {" ", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二", "十三", "十四","十五","十六", "十七","十八","十九", "二十",
-                    "二十一","二十二","二十三", "二十四", "二十五", "二十六", "二十七", "二十八", "二十九", "三十", "三十一"};
 
 int get_dayCode(int year) { 
     int daycode;
@@ -32,6 +30,7 @@ void setLeapYear(int year) {
 
 void print(int year, int daycode) {
     int month, day;
+    printf("\n\n日本語のカレンダー");
     for(month = 1; month <=12; month++) {
         printf("%s, %d", months[month], year);
         printf("\n\n日曜日 月曜日 火曜日 水曜日 木曜日 金曜日 土曜日");
@@ -43,7 +42,7 @@ void print(int year, int daycode) {
             printf("%2d", day);
 
             if(((day + daycode) % 7) > 0) {
-                printf("   ");
+                printf("     ");
             } else {
                 printf("\n ");
             }
@@ -54,12 +53,18 @@ void print(int year, int daycode) {
 
 int main(void) {
     int year, daycode, leap;
-
-    printf("四桁（けた）の年をタイプしてください： ");
+    printf("\n四桁（けた）の年をタイプしてください!");
+    printf("\nこのインプットは番号じゃなければ、プログラムがむげんるーぷです。。。 : ");
     scanf("%d", &year);
+    while(year < 1000 || year > 9999 || year < 0) {
+        printf("\nいいえ。この番号はタイプしてはいけません。。。");
+        printf("\n\n四桁（けた）の年をタイプしてください： ");
+        scanf("%d", &year);
+    }
     daycode = get_dayCode(year);
     setLeapYear(year);
     print(year, daycode);
     printf("\b");
+    printf("\n\nありがとうございます！！\n");
     return 0;
 }
